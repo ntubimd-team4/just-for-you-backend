9de99babb0c3fdf1d364e5a16d0d608952318266
Shan <Shan3program@gmail.com>
2023-05-29 06:45:24 +0800

    新增－帳號全查增加分頁

--- src/main/java/tw/edu/ntub/imd/justforyou/controller/UserAccountController.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/controller/UserAccountController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tw.edu.ntub.imd.justforyou.bean.UserAccountBean;
 import tw.edu.ntub.imd.justforyou.config.util.SecurityUtils;
+import tw.edu.ntub.imd.justforyou.databaseconfig.dto.Pager;
 import tw.edu.ntub.imd.justforyou.databaseconfig.enumerate.Role;
 import tw.edu.ntub.imd.justforyou.service.UserAccountService;
 import tw.edu.ntub.imd.justforyou.util.http.ResponseEntityBuilder;
@@ -20,22 +21,48 @@ import tw.edu.ntub.imd.justforyou.util.json.object.ObjectData;
 public class UserAccountController {
     private final UserAccountService userAccountService;
 
+    @GetMapping(path = "")
+    public ResponseEntity<String> getLoginUser() {
+        ObjectData objectData = new ObjectData();
+        userAccountService
+                .getById(SecurityUtils.getLoginUserAccount())
+                .ifPresentOrElse(
+                        userAccountBean -> {
+                            objectData.add("id", userAccountBean.getUserId());
+                            objectData.add("name", userAccountBean.getUserName());
+
+                            String role = SecurityUtils.getLoginUserRole();
+
+                            if (Role.isStudent(role)) {
+                                objectData.add("isStudent", true);
+                            } else if (Role.isTeacher(role)) {
+                                objectData.add("isTeacher", true);
+                            } else if (Role.isManage(role)) {
+                                objectData.add("isManage", true);
+                            }
+                        },
+                        () -> objectData.add("name", ""));
+        return ResponseEntityBuilder.success()
+                .message("查詢成功")
+                .data(objectData)
+                .build();
+    }
+
     @GetMapping(path = "/list")
-    public ResponseEntity<String> searchData() {
+    public ResponseEntity<String> searchAll(Pager pager) {
         ArrayData arrayData = new ArrayData();
-        for (UserAccountBean userAccountBean : userAccountService.searchData()) {
+        for (UserAccountBean userAccountBean : userAccountService.searchAll(pager)) {
             ObjectData objectData = arrayData.addObject();
             objectData.add("userId",userAccountBean.getUserId());
             objectData.add("userName",userAccountBean.getUserName());
             objectData.add("userSex",userAccountBean.getUserSex());
             objectData.add("department",userAccountBean.getDepartment());
-            objectData.add("department",userAccountBean.getDepartment());
-            objectData.add("googleId",userAccountBean.getGoogleId());
-            objectData.add("role", userAccountBean.getRole().ordinal());//問33.ordinal()
+            objectData.add("picture",userAccountBean.getPicture());
+            objectData.add("role", userAccountBean.getRole().getTypeName());
             objectData.add("available",userAccountBean.getAvailable());
         }
         return ResponseEntityBuilder.success()
-                .message("成功")
+                .message("查詢成功")
                 .data(arrayData)
                 .build();
     }
