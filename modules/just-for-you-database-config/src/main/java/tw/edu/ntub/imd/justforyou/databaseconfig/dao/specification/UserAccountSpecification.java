ed834feacf69939aed949413cc990e1a6f55cca4
Shan <Shan3program@gmail.com>
2023-05-30 16:22:18 +0800

    調整－帳號全查加類別、修改帳號啟用狀態程式碼

---
+++ modules/just-for-you-database-config/src/main/java/tw/edu/ntub/imd/justforyou/databaseconfig/dao/specification/UserAccountSpecification.java
@@ -0,0 +1,25 @@
+package tw.edu.ntub.imd.justforyou.databaseconfig.dao.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+import tw.edu.ntub.birc.common.util.StringUtils;
+import tw.edu.ntub.imd.justforyou.databaseconfig.entity.UserAccount;
+import tw.edu.ntub.imd.justforyou.databaseconfig.entity.UserAccount_;
+import tw.edu.ntub.imd.justforyou.databaseconfig.enumerate.Role;
+
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserAccountSpecification {
+    public Specification<UserAccount> checkBlank(String role) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (StringUtils.isNotBlank(role)) {
+                predicates.add(criteriaBuilder.equal(root.get(UserAccount_.ROLE), Role.of(role)));
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
--- src/main/java/tw/edu/ntub/imd/justforyou/service/UserAccountService.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/service/UserAccountService.java
@@ -2,9 +2,13 @@ package tw.edu.ntub.imd.justforyou.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 import tw.edu.ntub.imd.justforyou.bean.UserAccountBean;
-import tw.edu.ntub.imd.justforyou.databaseconfig.entity.UserAccount;
+import tw.edu.ntub.imd.justforyou.databaseconfig.dto.Pager;
+
+import java.util.List;
+
 
 public interface UserAccountService extends BaseService<UserAccountBean, String>, UserDetailsService {
+    List<UserAccountBean> searchData(String type, Pager pager);
 
     void updateAvailable(String userId);
 }
