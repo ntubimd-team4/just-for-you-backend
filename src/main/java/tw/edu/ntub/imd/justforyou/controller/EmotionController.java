03a9b5c6ccefa7a13dfef86e0b7054b9a1bc4c97
chienwenching <10946040@ntub.edu.tw>
2023-06-27 13:30:48 +0800

    新增 - 移除情緒標籤API

--- src/main/java/tw/edu/ntub/imd/justforyou/controller/EmotionController.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/controller/EmotionController.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tw.edu.ntub.imd.justforyou.bean.EmotionBean;
-import tw.edu.ntub.imd.justforyou.bean.UserAccountBean;
 import tw.edu.ntub.imd.justforyou.service.EmotionService;
 import tw.edu.ntub.imd.justforyou.util.http.ResponseEntityBuilder;
 
@@ -14,12 +13,23 @@ import tw.edu.ntub.imd.justforyou.util.http.ResponseEntityBuilder;
 @RequestMapping(path = "/emotion")
 public class EmotionController {
     private final EmotionService emotionService;
-    
+
+
+    //修改情緒
     @PatchMapping(path = "")
     public ResponseEntity<String> updateEmotion(@RequestBody EmotionBean emotionBean) {
         emotionService.update(Integer.valueOf(emotionBean.getEid()),emotionBean);
         return ResponseEntityBuilder.success()
                 .message("修改成功")
+                .build();
+    }
+
+    //情緒整筆刪除
+    @DeleteMapping(path = " ")
+    public ResponseEntity<String> deleteEmotion(@RequestBody EmotionBean deemotionBean) {
+        emotionService.delete(Integer.valueOf(deemotionBean.getEid()),deemotionBean);
+        return ResponseEntityBuilder.success()
+                .message("刪除成功")
                 .build();
     }
 
