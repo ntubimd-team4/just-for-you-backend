1e7fbbe15ae7393bde65e603d56cf68e1e1a7f67
Shan <Shan3program@gmail.com>
2023-07-13 17:23:13 +0800

    修正－刪除情緒標籤API

--- src/main/java/tw/edu/ntub/imd/justforyou/controller/EmotionController.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/controller/EmotionController.java
@@ -1,10 +1,10 @@
 package tw.edu.ntub.imd.justforyou.controller;
 
-
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tw.edu.ntub.imd.justforyou.bean.EmotionBean;
+import tw.edu.ntub.imd.justforyou.exception.NotFoundException;
 import tw.edu.ntub.imd.justforyou.service.EmotionService;
 import tw.edu.ntub.imd.justforyou.util.http.ResponseEntityBuilder;
 
@@ -24,15 +24,14 @@ public class EmotionController {
                 .build();
     }
 
-    //情緒整筆刪除
-    @DeleteMapping(path = " ")
-    public ResponseEntity<String> deleteEmotion(@RequestBody EmotionBean deemotionBean) {
-        emotionService.delete(Integer.valueOf(deemotionBean.getEid()),deemotionBean);
+    @DeleteMapping(path = "/{eId}")
+    public ResponseEntity<String> deleteEmotion(@PathVariable(name = "eId") Integer eId) {
+        EmotionBean emotionBean = emotionService.getById(eId)
+                .orElseThrow(() -> new NotFoundException("找不到資料, eId = " + eId));
+        emotionBean.setAvailable(!emotionBean.getAvailable());
+        emotionService.update(eId, emotionBean);
         return ResponseEntityBuilder.success()
                 .message("刪除成功")
                 .build();
     }
-
-
-
 }
