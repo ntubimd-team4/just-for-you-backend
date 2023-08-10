3a3da53adeae2190d886234e4902a64e22857983
Shan <Shan3program@gmail.com>
2023-08-10 14:20:11 +0800

    修正－32種情緒出現2個平靜問題

--- src/main/java/tw/edu/ntub/imd/justforyou/service/impl/SummaryRecordServiceImpl.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/service/impl/SummaryRecordServiceImpl.java
@@ -96,7 +96,7 @@ public class SummaryRecordServiceImpl extends BaseServiceImpl<SummaryRecordBean,
             throw new NotFoundException("請重新發送請求");
         }
 
-        String[] emotions = {"平靜", "快樂", "狂喜", "友愛", "接受", "信任", "平靜", "屈服", "擔心", "恐懼", "驚悚", "敬畏", "不解", "驚訝", "驚愕", "反對", "傷感", "悲傷", "悲痛", "懊悔", "厭倦", "厭惡", "憎恨", "鄙夷", "不耐煩", "生氣", "暴怒", "挑釁", "關心", "期待", "警惕", "樂觀"};
+        String[] emotions = {"平靜", "快樂", "狂喜", "友愛", "接受", "信任", "崇敬", "屈服", "擔心", "恐懼", "驚悚", "敬畏", "不解", "驚訝", "驚愕", "反對", "傷感", "悲傷", "悲痛", "懊悔", "厭倦", "厭惡", "憎恨", "鄙夷", "不耐煩", "生氣", "暴怒", "挑釁", "關心", "期待", "警惕", "樂觀"};
         List<String> emotionList = new ArrayList<>();
         for (String emotion : emotions) {
             if (emotionText.contains(emotion)) {
@@ -114,7 +114,7 @@ public class SummaryRecordServiceImpl extends BaseServiceImpl<SummaryRecordBean,
     }
 
     private CompletionRequest emotionRequest(String prompt) {
-        String mood = "平靜、快樂、狂喜、友愛、接受、信任、平靜、屈服、擔心、恐懼、驚悚、敬畏、不解、驚訝、驚愕、反對、傷感、悲傷、悲痛、懊悔、厭倦、厭惡、憎恨、鄙夷、不耐煩、生氣、暴怒、挑釁、關心、期待、警惕、樂觀";
+        String mood = "平靜、快樂、狂喜、友愛、接受、信任、崇敬、屈服、擔心、恐懼、驚悚、敬畏、不解、驚訝、驚愕、反對、傷感、悲傷、悲痛、懊悔、厭倦、厭惡、憎恨、鄙夷、不耐煩、生氣、暴怒、挑釁、關心、期待、警惕、樂觀";
         return CompletionRequest.builder()
                 .model("text-davinci-003")
                 .prompt(prompt + "請從以下" + mood + "這幾個情緒中判斷出這句話帶有哪些情緒")
