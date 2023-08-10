7becafa06477f051b3f753603496c37240c4c816
chienwenching <10946040@ntub.edu.tw>
2023-08-10 13:42:30 +0800

    新增 - 新增諮商紀錄

--- src/main/java/tw/edu/ntub/imd/justforyou/controller/ConsultationRecordController.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/controller/ConsultationRecordController.java
@@ -2,10 +2,14 @@ package tw.edu.ntub.imd.justforyou.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import tw.edu.ntub.imd.justforyou.bean.ConsultationRecordBean;
 import tw.edu.ntub.imd.justforyou.service.ConsultationRecordService;
+import tw.edu.ntub.imd.justforyou.util.http.BindingResultUtils;
 import tw.edu.ntub.imd.justforyou.util.http.ResponseEntityBuilder;
+
+import javax.validation.Valid;
 
 @AllArgsConstructor
 @RestController
@@ -18,6 +22,17 @@ public class ConsultationRecordController {
         consultationRecordService.update(consultationRecordBean.getCid() ,consultationRecordBean);
         return ResponseEntityBuilder.success()
                 .message("修改成功")
+                .build();
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<String> createStudent(@Valid @RequestBody ConsultationRecordBean consultationRecordBean,
+                                                BindingResult bindingResult) {
+        BindingResultUtils.validate(bindingResult);
+        consultationRecordService.save(consultationRecordBean);
+        return ResponseEntityBuilder
+                .success()
+                .message("新增成功")
                 .build();
     }
 }
