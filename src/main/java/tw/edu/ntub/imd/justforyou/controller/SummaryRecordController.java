7717b88d155e7e5df31b674ad467ed9c066a4162
chienwenching <10946040@ntub.edu.tw>
2023-08-30 13:50:58 +0800

    新增 - 搜尋摘要紀錄

--- src/main/java/tw/edu/ntub/imd/justforyou/controller/SummaryRecordController.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/controller/SummaryRecordController.java
@@ -3,13 +3,12 @@ package tw.edu.ntub.imd.justforyou.controller;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tw.edu.ntub.imd.justforyou.bean.SummaryRecordBean;
+import tw.edu.ntub.imd.justforyou.exception.NotFoundException;
 import tw.edu.ntub.imd.justforyou.service.SummaryRecordService;
 import tw.edu.ntub.imd.justforyou.util.http.ResponseEntityBuilder;
+import tw.edu.ntub.imd.justforyou.util.json.object.ObjectData;
 import tw.edu.ntub.imd.justforyou.util.json.object.SingleValueObjectData;
 
 import java.util.List;
@@ -46,6 +45,19 @@ public class SummaryRecordController {
         return ResponseEntityBuilder.success()
                 .message("摘要成功")
                 .data(SingleValueObjectData.create("value", remoteEnd))
+                .build();
+    }
+
+    @GetMapping(path = "", params = {"sid"})
+    public ResponseEntity<String> getSummaryRecord(@RequestParam(name = "sid") String id) {
+        ObjectData objectData = new ObjectData();
+        SummaryRecordBean summaryRecordBean = summaryRecordService.getById(Integer.valueOf(id)).orElseThrow(() -> new NotFoundException("查無此摘要，請確認是否正確"));
+        objectData.add("sid", summaryRecordBean.getSid());
+        objectData.add("userId", summaryRecordBean.getUserId());
+        objectData.add("summary", summaryRecordBean.getSummary());
+        return ResponseEntityBuilder.success()
+                .message("查詢成功")
+                .data(objectData)
                 .build();
     }
 }
