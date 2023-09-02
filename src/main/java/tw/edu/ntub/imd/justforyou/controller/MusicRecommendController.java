839d470d898c81069dc12684507ddb51ff59a2bb
Shan <Shan3program@gmail.com>
2023-09-02 23:14:21 +0800

    新增－音樂推薦Controller檔

---
+++ src/main/java/tw/edu/ntub/imd/justforyou/controller/MusicRecommendController.java
@@ -0,0 +1,16 @@
+package tw.edu.ntub.imd.justforyou.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tw.edu.ntub.imd.justforyou.service.MusicRecommendService;
+
+@Tag(name = "音樂推薦 /music-recommend")
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "/music-recommend")
+public class MusicRecommendController {
+    private final MusicRecommendService musicRecommendService;
+    
+}
