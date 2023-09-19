adff95c63a5a3d6e9774b3f150cce4e774a90962
Shan <Shan3program@gmail.com>
2023-09-19 17:01:39 +0800

    刪除－不必要import

--- src/main/java/tw/edu/ntub/imd/justforyou/service/impl/MusicRecommendServiceImpl.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/service/impl/MusicRecommendServiceImpl.java
@@ -3,27 +3,22 @@ package tw.edu.ntub.imd.justforyou.service.impl;
 import org.springframework.stereotype.Service;
 import tw.edu.ntub.imd.justforyou.bean.MusicRecommendBean;
 import tw.edu.ntub.imd.justforyou.databaseconfig.dao.MusicRecommendDAO;
-import tw.edu.ntub.imd.justforyou.databaseconfig.dao.specification.UserAccountSpecification;
 import tw.edu.ntub.imd.justforyou.databaseconfig.entity.MusicRecommend;
 import tw.edu.ntub.imd.justforyou.exception.NotFoundException;
 import tw.edu.ntub.imd.justforyou.service.MusicRecommendService;
 import tw.edu.ntub.imd.justforyou.service.transformer.MusicRecommendTransformer;
-import tw.edu.ntub.imd.justforyou.service.transformer.UserAccountTransformer;
 
 import java.util.Optional;
 
 @Service
 public class MusicRecommendServiceImpl extends BaseServiceImpl<MusicRecommendBean, MusicRecommend, Integer> implements MusicRecommendService {
     private final MusicRecommendDAO musicRecommendDAO;
-    private final UserAccountTransformer transformer;
-    private final UserAccountSpecification specification;
     private final MusicRecommendTransformer musicRecommendTransformer;
 
-    public MusicRecommendServiceImpl(MusicRecommendDAO musicRecommendDAO, MusicRecommendTransformer musicRecommendTransformer, UserAccountTransformer transformer, UserAccountSpecification specification) {
+    public MusicRecommendServiceImpl(MusicRecommendDAO musicRecommendDAO,
+                                     MusicRecommendTransformer musicRecommendTransformer) {
         super(musicRecommendDAO, musicRecommendTransformer);
         this.musicRecommendDAO = musicRecommendDAO;
-        this.transformer = transformer;
-        this.specification = specification;
         this.musicRecommendTransformer = musicRecommendTransformer;
     }
 
