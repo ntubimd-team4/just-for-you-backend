38219ea5696d5bf3dedc7e56a5df0d59da5a9e35
Shan <10946012@ntub.edu.tw>
2023-09-23 19:27:46 +0800

    修正－收藏狀態改至其他URL

---
+++ src/main/java/tw/edu/ntub/imd/justforyou/controller/CollectionController.java
@@ -0,0 +1,30 @@
+package tw.edu.ntub.imd.justforyou.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tw.edu.ntub.imd.justforyou.bean.MusicRecommendBean;
+import tw.edu.ntub.imd.justforyou.service.MusicRecommendService;
+import tw.edu.ntub.imd.justforyou.util.http.ResponseEntityBuilder;
+
+@Tag(name = "收藏 /collection")
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "/collection")
+public class CollectionController {
+    private final MusicRecommendService musicRecommendService;
+
+    @Operation(summary = "音樂收藏 - 修改收藏狀態")
+    @PatchMapping(path = "")
+    public ResponseEntity<String> updateCollection(@RequestBody MusicRecommendBean musicRecommendBean) {
+        musicRecommendService.updateCollection(musicRecommendBean.getRid());
+        return ResponseEntityBuilder.success()
+                .message("修改成功")
+                .build();
+    }
+}
--- src/main/java/tw/edu/ntub/imd/justforyou/controller/MusicRecommendController.java
+++ src/main/java/tw/edu/ntub/imd/justforyou/controller/MusicRecommendController.java
@@ -1,27 +1,12 @@
 package tw.edu.ntub.imd.justforyou.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tw.edu.ntub.imd.justforyou.bean.MusicRecommendBean;
-import tw.edu.ntub.imd.justforyou.service.MusicRecommendService;
-import tw.edu.ntub.imd.justforyou.util.http.ResponseEntityBuilder;
 
 @Tag(name = "音樂推薦 /music-recommend")
 @AllArgsConstructor
 @RestController
 @RequestMapping(path = "/music-recommend")
 public class MusicRecommendController {
-    private final MusicRecommendService musicRecommendService;
-
-    @Operation(summary = "音樂收藏 - 修改收藏狀態")
-    @PatchMapping(path = "/collection")
-    public ResponseEntity<String> updateCollection(@RequestBody MusicRecommendBean musicRecommendBean) {
-        musicRecommendService.updateCollection(musicRecommendBean.getRid());
-        return ResponseEntityBuilder.success()
-                .message("修改成功")
-                .build();
-    }
 }
