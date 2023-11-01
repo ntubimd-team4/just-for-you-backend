eb9be75e463aeb7fe52bee96852d446e479a6cd0
chienwenching <10946040@ntub.edu.tw>
2023-11-01 19:34:37 +0800

    新增 - TestEntity Annotation

--- modules/just-for-you-database-config/src/main/java/tw/edu/ntub/imd/justforyou/databaseconfig/entity/Test.java
+++ modules/just-for-you-database-config/src/main/java/tw/edu/ntub/imd/justforyou/databaseconfig/entity/Test.java
@@ -1,4 +1,17 @@
 package tw.edu.ntub.imd.justforyou.databaseconfig.entity;
 
+
+import lombok.Data;
+import tw.edu.ntub.imd.justforyou.databaseconfig.Config;
+import tw.edu.ntub.imd.justforyou.databaseconfig.entity.listener.SummaryRecordListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@EntityListeners(SummaryRecordListener.class)
+@Table(name = "summary_record", schema = Config.DATABASE_NAME)
 public class Test {
 }
